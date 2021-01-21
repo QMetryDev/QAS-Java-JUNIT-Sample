c5a168adfcaa7fac28ae869bb0afa3992f4b41a2
Arpit Khatri <arpit.khatri@qmetry.com>
2021-01-21 17:43:14 +0530

    Drag & Drop and Lamda Changes

--- src/test/java/base/common/StepsLibrary.java
+++ src/test/java/base/common/StepsLibrary.java
@@ -5,7 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
-
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,14 +16,59 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.edge.*;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.interactions.Actions;
 
 public class StepsLibrary {
-	
+	private static String getJsDndHelper() {
+		return "function simulateDragDrop(sourceNode, destinationNode) {\r\n" +
+				"		    var EVENT_TYPES = {\r\n" +
+				"		        DRAG_END: 'dragend',\r\n" +
+				"		        DRAG_START: 'dragstart',\r\n" +
+				"		        DROP: 'drop'\r\n" +
+				"		    }\r\n" +
+				"		\r\n" +
+				"		    function createCustomEvent(type) {\r\n" +
+				"		        var event = new CustomEvent(\"CustomEvent\")\r\n" +
+				"		        event.initCustomEvent(type, true, true, null)\r\n" +
+				"		        event.dataTransfer = {\r\n" +
+				"		            data: {\r\n" +
+				"		            },\r\n" +
+				"		            setData: function(type, val) {\r\n" +
+				"		                this.data[type] = val\r\n" +
+				"		            },\r\n" +
+				"		            getData: function(type) {\r\n" +
+				"		                return this.data[type]\r\n" +
+				"		            }\r\n" +
+				"		        }\r\n" +
+				"		        return event\r\n" +
+				"		    }\r\n" +
+				"		\r\n" +
+				"		    function dispatchEvent(node, type, event) {\r\n" +
+				"		        if (node.dispatchEvent) {\r\n" +
+				"		            return node.dispatchEvent(event)\r\n" +
+				"		        }\r\n" +
+				"		        if (node.fireEvent) {\r\n" +
+				"		            return node.fireEvent(\"on\" + type, event)\r\n" +
+				"		        }\r\n" +
+				"		    }\r\n" +
+				"		\r\n" +
+				"		    var event = createCustomEvent(EVENT_TYPES.DRAG_START)\r\n" +
+				"		    dispatchEvent(sourceNode, EVENT_TYPES.DRAG_START, event)\r\n" +
+				"		\r\n" +
+				"		    var dropEvent = createCustomEvent(EVENT_TYPES.DROP)\r\n" +
+				"		    dropEvent.dataTransfer = event.dataTransfer\r\n" +
+				"		    dispatchEvent(destinationNode, EVENT_TYPES.DROP, dropEvent)\r\n" +
+				"		\r\n" +
+				"		    var dragEndEvent = createCustomEvent(EVENT_TYPES.DRAG_END)\r\n" +
+				"		    dragEndEvent.dataTransfer = event.dataTransfer\r\n" +
+				"		    dispatchEvent(sourceNode, EVENT_TYPES.DRAG_END, dragEndEvent)\r\n" +
+				"		}";
+	}
 	public static void customStep(WebDriver driver) {
 			 //Custom steps
-			//  driver.get("http://www.google.com"); 
+			//  driver.get("http://www.google.com");
 	}
-	
+
 	public static void implicitWait(long time) {
 		try {
 			Thread.sleep(time);
@@ -30,8 +76,25 @@ public class StepsLibrary {
 			System.out.println(e);
 		}
 	}
-	
+
 	public static void maximizeWindow(WebDriver driver) {
 		driver.manage().window().maximize();
 	}
+
+    public static void dragAndDropPerform(WebDriver driver,WebElement source, WebElement target) {
+		((JavascriptExecutor) driver).executeScript(getJsDndHelper() + "simulateDragDrop(arguments[0], arguments[1])",source,target);
+		Actions actions = new Actions(driver);
+		actions.dragAndDrop(source,target).build().perform();
+	}
+
+	public static void dragAndDropOnAndPerform(WebDriver driver, WebElement source, String Xtarget, String Ytarget) {
+		Actions actions = new Actions(driver);
+		actions.dragAndDropBy(source,Integer.parseInt(Xtarget), Integer.parseInt(Ytarget)).build().perform();
+	}
+
+	public static void dragAndDropOnValuePerform(WebDriver driver, WebElement source, String value) {
+		String executScriptValue ="arguments[0].setAttribute('value',"+Integer.parseInt(value)+");if(typeof(arguments[0].onchange) === 'function'){arguments[0].onchange('');}";
+		((JavascriptExecutor) driver).executeScript(executScriptValue,source);
+	}
+
 }
